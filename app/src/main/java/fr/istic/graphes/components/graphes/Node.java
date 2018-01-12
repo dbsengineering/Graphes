27bf57a0907bdb79a0d5edb47bf2e244ec75d00d
DbsEngineering <piranna56@yahoo.fr>
2018-01-12 19:22:53 +0100

    Update Node.java

--- app/src/main/java/fr/istic/graphes/components/graphes/Node.java
+++ app/src/main/java/fr/istic/graphes/components/graphes/Node.java
@@ -7,23 +7,20 @@
  *		Formation : ....... Master 1 MIAGE							*
  *		Lecture : ......... MOBILE									*
  *		Group : ........... 1a										*
- *		Authors : ......... Cavron Jérémy, Ez Ziraiy Nada			*
+ *		Authors : ......... Cavron Jérémy                   		*
  *		DateStart : ....... 19/09/2017								*
  *		DateModify : ...... 09/11/2017								*
  *******************************************************************/
-package bzh.dbs.graph.components.graphes;
+package fr.istic.graphes.components.graphes;
 
 import android.graphics.PointF;
 import android.graphics.RectF;
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import java.io.Serializable;
 
 /**
  *
  */
-public class Node implements Parcelable, Serializable {
+public class Node implements Serializable {
 
     //--- Déclaration des propriétées ---
     private static final long serialVersionUID = -29238982928393L;
@@ -188,7 +185,7 @@ public class Node implements Parcelable, Serializable {
     }
 
     /**
-     *
+     * Procédure de ré-initialisation après une sérialisation.
      */
     public void reinit(){
         int txtSize = this.nameNode.length();//Taille du texte
@@ -220,123 +217,5 @@ public class Node implements Parcelable, Serializable {
                 -(this.coordX-(this.size+(txtSize*mult))))/2);
         this.pMiddle.y = (this.coordY-this.size) + (((this.coordY+this.size)
                 -(this.coordY-this.size))/2);
-    }
-
-    /**
-     * Fonction qui permet d'écrire le contenu du Parcel.
-     * @return
-     */
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    /**
-     * Procédure qui permet de parceler l'objet.
-     * @param dest : La parcelle dans laquelle l'objet doit être écrit. Parcel.
-     * @param flags : Drapeaux supplémentaires sur la façon dont l'objet doit être écrit.
-     *              Peut être 0 ou PARCELABLE_WRITE_RETURN_VALUE.
-     */
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-
-        //float x = this.pMiddle.x;
-        //float y = this.pMiddle.y;
-
-        /*float recInLeft = this.rectFInt.left;
-        float recInRight = this.rectFInt.right;
-        float recInTop = this.rectFInt.top;
-        float recInBottom = this.rectFInt.bottom;
-
-        float recOutLeft = this.rectFOut.left;
-        float recOutRight = this.rectFOut.right;
-        float recOutTop = this.rectFOut.top;
-        float recOutBottom = this.rectFOut.bottom;*/
-
-        //dest.writeFloat(x);
-        //dest.writeFloat(y);
-
-       /* dest.writeFloat(recInLeft);
-        dest.writeFloat(recInRight);
-        dest.writeFloat(recInTop);
-        dest.writeFloat(recInBottom);
-
-        dest.writeFloat(recOutLeft);
-        dest.writeFloat(recOutRight);
-        dest.writeFloat(recOutTop);
-        dest.writeFloat(recOutBottom);*/
-
-        dest.writeString(this.nameNode);
-        dest.writeString(this.colorInt);
-        dest.writeString(this.colorTxt);
-        dest.writeInt(this.id);
-        dest.writeInt(this.size);
-
-        dest.writeFloat(this.coordX);
-        dest.writeFloat(this.coordY);
-    }
-
-    /**
-     * Création d’un objet CREATOR de la classe Parcelable
-     */
-    public static final Parcelable.Creator<Node> CREATOR = new Parcelable.Creator<Node>() {
-        /**
-         *
-         * @param in
-         * @return
-         */
-        public Node createFromParcel(Parcel in) {
-            return new Node(in);
-        }
-
-        /**
-         *
-         * @param size
-         * @return
-         */
-        public Node[] newArray(int size) {
-            return new Node[size];
-        }
-    };
-
-    /**
-     * Constructeur pour le parcel. Propriétées dans l'ordre d'écriture.
-     * @param in Parcel
-     */
-    public Node(Parcel in){
-        //float x = in.readFloat();
-        //float y = in.readFloat();
-
-       /* float recInLeft = in.readFloat();
-        float recInRight = in.readFloat();
-        float recInTop = in.readFloat();
-        float recInBottom = in.readFloat();
-
-        float recOutLeft = in.readFloat();
-        float recOutRight = in.readFloat();
-        float recOutTop = in.readFloat();
-        float recOutBottom = in.readFloat();*/
-
-        //this.pMiddle.x = x;
-        //this.pMiddle.y = y;
-
-        /*this.rectFInt.left = recInLeft;
-        this.rectFInt.right = recInRight;
-        this.rectFInt.top = recInTop;
-        this.rectFInt.bottom = recInBottom;
-
-        this.rectFOut.left = recOutLeft;
-        this.rectFOut.right = recOutRight;
-        this.rectFOut.top = recOutTop;
-        this.rectFOut.bottom = recOutBottom;*/
-
-        this.nameNode = in.readString();
-        this.colorInt = in.readString();
-        this.colorTxt = in.readString();
-        this.id = in.readInt();
-        this.size = in.readInt();
-
-        this.coordX = in.readFloat();
-        this.coordY = in.readFloat();
     }
 }
